7fef862384dccd23ca5d192ea9844ca91198f762
Hassan Khousheish <khousheish@live.com>
2024-11-23 23:01:32 +0100

    Fix Fragment Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { RouterModule, Router, Scroll } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 import { environment } from '@Environment';
 import { MenubarModule } from 'primeng/menubar';
@@ -13,15 +15,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [MenubarModule, CommonModule, AvatarModule, CardModule],
+  imports: [MenubarModule, CommonModule, AvatarModule, CardModule, RouterModule],
 })
 export class AppComponent implements OnInit {
   items: MenuItem[] = [];
 
   constructor(
     private readonly translateService: TranslateService,
-    private readonly elementRef: ElementRef
-    ) { }
+    private router: Router,
+  ) {
+    this.router.events.pipe(filter((e) => e instanceof Scroll)).subscribe((e: any) => {
+      if (e.anchor) {
+        setTimeout(() => {
+          const element = document.getElementById(e.anchor);
+          if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+          }
+        }, 0);
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.translateService.setDefaultLang(environment.defaultLanguage);
@@ -31,20 +44,27 @@ export class AppComponent implements OnInit {
       {
         label: 'Work',
         icon: 'pi pi-briefcase',
-        fragment: 'work'
+        routerLink: ['.'],
+        fragment: 'work',
       },
       {
         label: 'Blog',
-        icon: 'pi pi-pen-to-square'
+        icon: 'pi pi-pen-to-square',
+        routerLink: ['.'],
+        fragment: 'blog',
       },
       {
         label: 'Podcast',
-        icon: 'pi pi-microphone'
+        icon: 'pi pi-microphone',
+        routerLink: ['.'],
+        fragment: 'podcast',
       },
       {
         label: 'Contact',
         icon: 'pi pi-envelope',
-      }
+        routerLink: ['.'],
+        fragment: 'contact',
+      },
     ];
   }
 }
