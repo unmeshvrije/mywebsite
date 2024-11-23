edc8f7e473212f11f647a1ad3eeb9ed26f06f6db
unmeshvrije <unmesh.joshi126@gmail.com>
2024-11-23 21:29:24 +0100

    Initial Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 import { environment } from '@Environment';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { CardModule } from 'primeng/card';
+import { AvatarModule } from 'primeng/avatar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ngt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MenubarModule, CommonModule, AvatarModule, CardModule],
 })
 export class AppComponent implements OnInit {
-  constructor(private readonly translateService: TranslateService) {}
+  items: MenuItem[] = [];
+
+  constructor(
+    private readonly translateService: TranslateService,
+    private readonly elementRef: ElementRef
+    ) { }
 
   ngOnInit(): void {
     this.translateService.setDefaultLang(environment.defaultLanguage);
 
     this.translateService.use(environment.defaultLanguage);
+    this.items = [
+      {
+        label: 'Work',
+        icon: 'pi pi-briefcase',
+        fragment: 'work'
+      },
+      {
+        label: 'Blog',
+        icon: 'pi pi-pen-to-square'
+      },
+      {
+        label: 'Podcast',
+        icon: 'pi pi-microphone'
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+      }
+    ];
   }
 }
